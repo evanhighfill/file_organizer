e0c6cecca3eed3dbd0bdb0a3517501d041ffc272
Evan Highfill <emhighfill18@gmail.com>
2019-12-01 15:01:18 -0600

    Added ability to copy or move

--- mover.sh
+++ mover.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
 INPUT_PATH=F:/DCIM/temp
-OUTPUT_PATH=test
+OUTPUT_PATH=.
 VIDEO_PATH=$OUTPUT_PATH/videos
 IMAGE_PATH=$OUTPUT_PATH/pictures
 VIDEO_EXTENSION=*.MOV
 IMAGE_EXTENSION=*.JPG
 
-function moveVideos () {
+function processVideos () {
     mkdir -p $VIDEO_PATH
-    mv $INPUT_PATH/$VIDEO_EXTENSION -t $VIDEO_PATH
+    $cmd -i $INPUT_PATH/$VIDEO_EXTENSION -t $VIDEO_PATH
 }
-function moveImages () {
+function processImages () {
     mkdir -p $IMAGE_PATH
-    mv $INPUT_PATH/$IMAGE_EXTENSION -t $IMAGE_PATH
+    $cmd -i $INPUT_PATH/$IMAGE_EXTENSION -t $IMAGE_PATH
 }
-function moveBoth () {
-    moveVideos
-    moveImages
+function processBoth () {
+    processVideos
+    processImages
+}
+
+function moveOrCopy () {
+# moveOrCopy sets the value of $cmd.  If its empty call the function because it hasn't been set.
+    while [ true ]
+    do
+        echo "Do you want to copy or move the files? Enter (M) or (C) or (CNTL + C) to quit."
+        read cmdInput
+        if [ $cmdInput = "M" ] || [ $cmdInput = "m" ]; then
+            cmd=mv
+            break
+        elif [ $cmdInput = "C" ] || [ $cmdInput = "c" ]; then
+            cmd=cp
+            break
+        else
+            echo "You've selected the wrong option dingus." 
+        fi
+    done
 }
 
 while [ true ]
@@ -28,13 +46,17 @@ do
     echo "3. Move Both"
     echo "CNTL + C to quit"
     read response
-    if [ $response = 1 ]; then
-        moveVideos
-    elif [ $response = 2 ]; then
-        moveImages 
-    elif [ $response = 3 ]; then
-        moveBoth
+    if [ $response = 1 ] || [ $response = 2 ] || [ $response = 3 ]; then
+        moveOrCopy
     else
         echo "You've selected the wrong option dingus." 
+    fi
+
+    if [ $response = 1 ]; then
+        processVideos
+    elif [ $response = 2 ]; then
+        processImages 
+    elif [ $response = 3 ]; then
+        processBoth
     fi
 done 
